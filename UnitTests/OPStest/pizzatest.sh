9d43ee97fe8f1788eb2d2a436fe2a1bd84801721
pane <sniqer@users.noreply.github.com>
2016-04-06 16:16:45 +0200

    printing exit status from pizzatest.sh

--- UnitTests/OPStest/pizzatest.sh
+++ UnitTests/OPStest/pizzatest.sh
@@ -2,20 +2,45 @@
 
 cd ../../
 
+count=0
+exit_normal=0
+exit_tcp=0
+exit_udp=0
 #remove gcda files build folder
 find build.debug/ -name '*.gcda' | xargs rm -f
 
-date > Unit_test_results/UnitTests-result.txt
-build.debug/UnitTests/OPStest/UnitTests/test-serialize-and-deserialize >> Unit_test_results/UnitTests-result.txt
+date > Unit_test_results/UnitTests-result.txt 
+build.debug/UnitTests/OPStest/UnitTests/test-serialize-and-deserialize >> Unit_test_results/UnitTests-result.txt ; count=$(($count + $?))
 
 build.debug/UnitTests/OPStest/UnitTests/test-subscribe    >> Unit_test_results/UnitTests-result.txt &
+pid_normal=$!
+
 build.debug/UnitTests/OPStest/UnitTests/test-subscribeTCP >> Unit_test_results/UnitTests-result.txt &
+pid_tcp=$!
+
 build.debug/UnitTests/OPStest/UnitTests/test-subscribeUDP >> Unit_test_results/UnitTests-result.txt &
+pid_udp=$!
 sleep 1
 build.debug/UnitTests/OPStest/src/publishBin              >> Unit_test_results/UnitTests-result.txt
 
-mkdir -p Unit_test_results/Coverage
-gcovr -r . --html --html-details -o Unit_test_results/Coverage/coverage_result_OPS.html
+wait $pid_normal
+exit_normal=$?
 
-cppcheck --enable=all Cpp/source Cpp/include 2> Unit_test_results/SA_ERRORS.txt > /dev/null
-echo "Done!"
+wait $pid_tcp
+exit_tcp=$?
+
+wait $pid_normal
+exit_udp=$?
+
+mkdir -p Unit_test_results/Coverage
+#gcovr -r . --html --html-details -o Unit_test_results/Coverage/coverage_result_OPS.html
+
+#cppcheck --enable=all Cpp/source Cpp/include 2> Unit_test_results/SA_ERRORS.txt > /dev/null
+count=$(( $count + $exit_normal + $exit_tcp  + $exit_udp ))
+
+if [ $count == 0 ]
+then
+	printf "\n\n   ALL TEST PASSED\n\n\n"
+else
+	printf "\n\n   $count TEST FAILED\n\n\n"
+fi
