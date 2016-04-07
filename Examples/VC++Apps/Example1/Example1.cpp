e581e963e68d33ee7c6f43dd7b8d6f7d1eba894b
Lennart Andersson <LelleAnd@users.noreply.github.com>
2016-04-07 22:04:06 +0200

    Examples: Updated C++ Example1

--- Examples/VC++Apps/Example1/Example1.cpp
+++ Examples/VC++Apps/Example1/Example1.cpp
@@ -26,27 +26,27 @@ using namespace TestAll;
 
 void PublisherExample()
 {
-	// Create OPS participant to access a domain in the default configuration file "ops_config.xml" in current working directory
-	// There are other calls to create a participant for a specific configuration file
+	// Create OPS participant to access a domain in the default configuration file
+	// "ops_config.xml" in current working directory. There are other overloads to
+	// create a participant for a specific configuration file.
 	ops::Participant* participant = ops::Participant::getInstance("TestAllDomain");
 
 	// Add our generated factory so OPS can create our data objects
 	participant->addTypeSupport(new TestAll::TestAllTypeFactory());
 
-	// Add an errorwriter instance to the participant to catch eventual internal OPS errors
+	// Add an errorwriter instance to the participant to catch ev. internal OPS errors
 	// We can easily write our own if we want to log data in another way.
 	ops::ErrorWriter* errorWriter = new ops::ErrorWriter(std::cout);
 	participant->addListener(errorWriter);
-
 
 	// Create the topic to publish on, might throw ops::NoSuchTopicException
 	// The topic must exist in the used ops configuration file
 	ops::Topic topic = participant->createTopic("ChildTopic");
 
-	std::cout << "Publishing on " << topic.getName() << 
-		" [" << topic.getTransport() << 
+	std::cout << "Publishing on " << topic.getName() <<
+		" [" << topic.getTransport() <<
 		"::" << topic.getDomainAddress() <<
-		"::" << topic.getPort() << 
+		"::" << topic.getPort() <<
 		"] " << std::endl;
 
 
@@ -56,8 +56,8 @@ void PublisherExample()
 	// It's a good practise to set a publisher name, but it's not necessary.
 	pub.setName("TestAllPublisher");
 
-	// If there are many publishers on the same topic, a key can be set to identify the specific publisher instance.
-	// A subscriber can tell OPS to filter on this key.
+	// If there are many publishers on the same topic, a key can be set to identify a
+	// specific publisher instance. A subscriber can tell OPS to filter on this key.
 	pub.setKey("InstanceOne");
 
 
@@ -80,6 +80,64 @@ void PublisherExample()
 
 /// =======================================================================
 
+void PollingSubscriberExample()
+{
+	// Create OPS participant to access a domain in the default configuration file
+	// "ops_config.xml" in current working directory. There are other overloads to
+	// create a participant for a specific configuration file.
+	ops::Participant* participant = ops::Participant::getInstance("TestAllDomain");
+
+	// Add our generated factory so OPS can create our data objects
+	participant->addTypeSupport(new TestAll::TestAllTypeFactory());
+
+	// Add an errorwriter instance to the participant to catch ev. internal OPS errors
+	// We can easily write our own if we want to log data in another way.
+	ops::ErrorWriter* errorWriter = new ops::ErrorWriter(std::cout);
+	participant->addListener(errorWriter);
+
+	// Create the topic to subscribe to, might throw ops::NoSuchTopicException
+	// The topic must exist in the used ops configuration file
+	ops::Topic topic = participant->createTopic("ChildTopic");
+
+	std::cout << "Subscribing to " << topic.getName() <<
+		" [" << topic.getTransport() <<
+		"::" << topic.getDomainAddress() <<
+		"::" << topic.getPort() <<
+		"] " << std::endl;
+
+
+	// Create a subscriber for ChildData
+	ChildDataSubscriber sub(topic);
+
+	// Setup any filters ...
+
+	// Finally start the subscriber (tell it to start listening for data)
+	sub.start();
+
+	while (true) {
+#ifdef zzz
+		if (sub.waitForNewData(100)) {
+			// Need to lock message while using it's data via the reference
+			sub.aquireMessageLock();
+			ChildData* data = (ChildData*)sub.getDataReference();
+			std::cout << "New data found: Received ChildTopic with " << data->l << std::endl;
+			sub.releaseMessageLock();
+		}
+#else
+		if (sub.newDataExist()) {
+			ChildData data;
+			// Message lock is handled internaly in subscriber
+			sub.getData(&data);
+			std::cout << "New data found: Received ChildTopic with " << data.l << std::endl;
+		}
+		Sleep(10);
+#endif
+	}
+
+}
+
+/// =======================================================================
+
 class SubscriptionHandler : ops::DataListener, ops::DeadlineMissedListener
 {
 private:
@@ -87,28 +145,27 @@ private:
 public:
 	SubscriptionHandler()
 	{
-		// Create OPS participant to access a domain in the default configuration file "ops_config.xml" 
-		// in current working directory.
-		// There are other calls to create a participant for a specific configuration file
+		// Create OPS participant to access a domain in the default configuration file
+		// "ops_config.xml" in current working directory. There are other overloads to
+		// create a participant for a specific configuration file.
 		ops::Participant* participant = ops::Participant::getInstance("TestAllDomain");
 
 		// Add our generated factory so OPS can create our data objects
 		participant->addTypeSupport(new TestAll::TestAllTypeFactory());
 
-		// Add an errorwriter instance to the participant to catch eventual internal OPS errors
+		// Add an errorwriter instance to the participant to catch ev. internal OPS errors
 		// We can easily write our own if we want to log data in another way.
 		ops::ErrorWriter* errorWriter = new ops::ErrorWriter(std::cout);
 		participant->addListener(errorWriter);
-
 
 		// Create the topic to subscribe to, might throw ops::NoSuchTopicException
 		// The topic must exist in the used ops configuration file
 		ops::Topic topic = participant->createTopic("ChildTopic");
 
-		std::cout << "Subscribing to " << topic.getName() << 
-			" [" << topic.getTransport() << 
+		std::cout << "Subscribing to " << topic.getName() <<
+			" [" << topic.getTransport() <<
 			"::" << topic.getDomainAddress() <<
-			"::" << topic.getPort() << 
+			"::" << topic.getPort() <<
 			"] " << std::endl;
 
 
@@ -137,8 +194,10 @@ public:
 	void onNewData(ops::DataNotifier* subscriber)
 	{
 		// NOTE: It's important that we keep this callback fast, it will block
-		// the receive for all topics belonging to the participant (currently a single 
+		// the receive for all topics belonging to the participant (currently a single
 		// thread for each participant instance, that does all receive handling).
+
+		// Message lock is held while in callback
 
 		// If the class subscribes for more than one topic, all will come here
 		// So check which it is
@@ -148,21 +207,21 @@ public:
 			// eg. publisher name, publication id (message counter), ...
 			// These may be of interrest
 			ops::OPSMessage* newMess = sub->getMessage();
-			
+
 			// Get the actual data object published
 			ChildData* data = (ChildData*)newMess->getData();
 
 			// Use the data
 			std::cout << "Received ChildTopic with " << data->l << std::endl;
 
-			// NOTE that the OPSMessage instance and the data object, as default 
-			// will be deleted when this callback returns. 
+			// NOTE that the OPSMessage instance and the data object, as default
+			// will be deleted when this callback returns.
 			// If you eg. want to buffer messages to keep the callback fast, you can
 			// postpone the delete by calling "newMess->reserve()" here in the callback.
 			// When you are finished with the message, call "newMess->unreserve()".
-			// This will delete the message if the reserve count == 0 (ie. if the number 
-			// of reserve() and unreserv() calls match.
-			
+			// This will delete the message if the reserve count == 0 (ie. if the number
+			// of reserve() and unreserve() calls match.
+
 		}
 	}
 
@@ -173,7 +232,7 @@ public:
 	}
 };
 
-void SubscriberExample()
+void CallbackSubscriberExample()
 {
 	SubscriptionHandler handler;
 
@@ -185,7 +244,7 @@ void SubscriberExample()
 void usage()
 {
 	std::cout << "" << std::endl;
-	std::cout << "Usage: Example1 pub|sub" << std::endl;
+	std::cout << "Usage: Example1 pub|sub_poll|sub_callback" << std::endl;
 	std::cout << "" << std::endl;
 }
 
@@ -195,8 +254,10 @@ int main(int argc, char**argv)
 		std::string arg(argv[1]);
 		if (arg == "pub") {
 			PublisherExample();
-		} else if (arg == "sub"){
-			SubscriberExample();		
+		} else if (arg == "sub_callback"){
+			CallbackSubscriberExample();
+		} else if (arg == "sub_poll"){
+			PollingSubscriberExample();
 		} else {
 			usage();
 		}
@@ -204,5 +265,3 @@ int main(int argc, char**argv)
 		usage();
 	}
 }
-
-
