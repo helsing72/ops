583f154f9d0b935c8d9d978b728143ea0149cf62
pane <sniqer@users.noreply.github.com>
2016-04-08 16:34:34 +0200

    minor changes pizzatest-xml.sh

--- UnitTests/OPStest/pizzatest-xml.sh
+++ UnitTests/OPStest/pizzatest-xml.sh
@@ -6,8 +6,8 @@ JENK_DIR="/tmp/jenkins/workspace/OPS_PizzaTest"
 UNIT_DIR="Unit_test_results/"
 build.debug/UnitTests/OPStest/UnitTests/test-serialize-and-deserialize --gtest_output="xml:$JENK_DIR/Unittest-serialize-and-deserialize_result.xml"
 
-build.debug/UnitTests/OPStest/UnitTests/test-subscribe  	--gtest_output="xml:$JENK_DIR/Unittest-publish-and-subscribe_result.xml"     &
-build.debug/UnitTests/OPStest/UnitTests/test-subscribeTCP	--gtest_output="xml:$JENK_DIR/Unittest-publish-and-subscribe_result_tcp.xml" &
+#build.debug/UnitTests/OPStest/UnitTests/test-subscribe  	--gtest_output="xml:$JENK_DIR/Unittest-publish-and-subscribe_result.xml"     &
+#build.debug/UnitTests/OPStest/UnitTests/test-subscribeTCP	--gtest_output="xml:$JENK_DIR/Unittest-publish-and-subscribe_result_tcp.xml" &
 build.debug/UnitTests/OPStest/UnitTests/test-subscribeUDP  	--gtest_output="xml:$JENK_DIR/Unittest-publish-and-subscribe_result_udp.xml" &
 
 #build.debug/UnitTests/OPStest/UnitTests/test-subscribe  	--gtest_output="xml:$UNIT_DIR/Unittest-publish-and-subscribe_result.xml"     &
@@ -17,6 +17,7 @@ build.debug/UnitTests/OPStest/UnitTests/test-subscribeUDP  	--gtest_output="xml:
 sleep 1
 build.debug/UnitTests/OPStest/src/publishBin
 
+sleep 10
 mkdir -p Unit_test_results/Coverage
 gcovr --root . -b --xml --xml-pretty -o $JENK_DIR/coverage_result_OPS_jenkins.xml
 
