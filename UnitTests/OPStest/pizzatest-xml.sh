7ba1ce9c4778696cd4f3bf4b064dc83c71e2d629
pane <sniqer@users.noreply.github.com>
2016-04-11 10:58:52 +0200

    minor changes to pizzatest.sh

--- UnitTests/OPStest/pizzatest-xml.sh
+++ UnitTests/OPStest/pizzatest-xml.sh
@@ -3,7 +3,6 @@
 cd ../..
 JENK_DIR="/tmp/jenkins/workspace/ops_pizzatest"
 
-echo "denna text skall skrivas ut i klartext!!!"
 build.debug/UnitTests/OPStest/UnitTests/test-serialize-and-deserialize --gtest_output="xml:$JENK_DIR/Unittest-serialize-and-deserialize_result.xml"
 
 build.debug/UnitTests/OPStest/UnitTests/test-subscribe  	--gtest_output="xml:$JENK_DIR/Unittest-publish-and-subscribe_result.xml"     &
@@ -14,7 +13,7 @@ build.debug/UnitTests/OPStest/UnitTests/test-subscribeUDP  	--gtest_output="xml:
 sleep 1
 build.debug/UnitTests/OPStest/src/publishBin
 
-sleep 10
+sleep 3
 gcovr --root . -b --xml --xml-pretty -o $JENK_DIR/coverage_result_OPS_jenkins.xml
 
 
