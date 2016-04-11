5db9023c3a30bed09695cc5dd8adb9ce7783d62e
pane <sniqer@users.noreply.github.com>
2016-04-11 10:55:44 +0200

    minor changes pizzatest.sh

--- UnitTests/OPStest/pizzatest-xml.sh
+++ UnitTests/OPStest/pizzatest-xml.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-#cd /lkp/nlt/dev/users/pane/nlt/OPS4/ops4
 cd ../..
-JENK_DIR="/tmp/jenkins/workspace/OPS_PizzaTest"
-UNIT_DIR="Unit_test_results/"
+JENK_DIR="/tmp/jenkins/workspace/ops_pizzatest"
+
+echo "denna text skall skrivas ut i klartext!!!"
 build.debug/UnitTests/OPStest/UnitTests/test-serialize-and-deserialize --gtest_output="xml:$JENK_DIR/Unittest-serialize-and-deserialize_result.xml"
 
 build.debug/UnitTests/OPStest/UnitTests/test-subscribe  	--gtest_output="xml:$JENK_DIR/Unittest-publish-and-subscribe_result.xml"     &
 build.debug/UnitTests/OPStest/UnitTests/test-subscribeTCP	--gtest_output="xml:$JENK_DIR/Unittest-publish-and-subscribe_result_tcp.xml" &
 build.debug/UnitTests/OPStest/UnitTests/test-subscribeUDP  	--gtest_output="xml:$JENK_DIR/Unittest-publish-and-subscribe_result_udp.xml" &
 
-#build.debug/UnitTests/OPStest/UnitTests/test-subscribe  	--gtest_output="xml:$UNIT_DIR/Unittest-publish-and-subscribe_result.xml"     &
-#build.debug/UnitTests/OPStest/UnitTests/test-subscribeTCP	--gtest_output="xml:$UNIT_DIR/Unittest-publish-and-subscribe_result_tcp.xml" &
-#build.debug/UnitTests/OPStest/UnitTests/test-subscribeUDP  	--gtest_output="xml:$UNIT_DIR/Unittest-publish-and-subscribe_result_udp.xml" &
 
 sleep 1
 build.debug/UnitTests/OPStest/src/publishBin
 
 sleep 10
-mkdir -p Unit_test_results/Coverage
 gcovr --root . -b --xml --xml-pretty -o $JENK_DIR/coverage_result_OPS_jenkins.xml
 
 
