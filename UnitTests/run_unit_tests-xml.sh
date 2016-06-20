72985f688dd5585d958c291277c955016e9f2f80
pane <pane@dynamics.saab.se>
2016-06-20 16:34:52 +0200

    ...

--- UnitTests/run_unit_tests-xml.sh
+++ UnitTests/run_unit_tests-xml.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -l
 
-#cd OPStest-C++
-#./pizzatest-xml.sh
-echo
-echo "ls in run_unit_tests-xml.sh"
-ls
 cd OPStest-Python/
-echo
-echo "ls in run_unit_tests-xml.sh"
-ls
+
 ./pizzatest-xml.sh
