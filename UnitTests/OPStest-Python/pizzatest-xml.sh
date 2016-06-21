352a730bedb76d7ac7790553cae4b562785188c7
pane <pane@dynamics.saab.se>
2016-06-21 08:18:03 +0200

    ...

--- UnitTests/OPStest-Python/pizzatest-xml.sh
+++ UnitTests/OPStest-Python/pizzatest-xml.sh
@@ -9,11 +9,9 @@ export PYTHONPATH=$PYTHONPATH:"../../Python"
 echo $PYTHONPATH
 
 
-python -m pytest -rw UnitTests/test_serializeAndDeserialize.py #--junitxml="$JENK_DIR/unittest-python-ser-deser-result.xml"
+python -m pytest -rw UnitTests/test_serializeAndDeserialize.py --junitxml="$JENK_DIR/unittest-python-ser-deser-result.xml"
 
-python -m pytest -rw UnitTests/test_subscribe.py &
-#--junitxml="$JENK_DIR/unittest-python-pub-sub-result.xml" &
-
+python -m pytest -rw UnitTests/test_subscribe.py --junitxml="$JENK_DIR/unittest-python-pub-sub-result.xml" &
 
 sleep 1
 python  UnitTests/PublishData.py
