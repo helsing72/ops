d9b4c0939df67650242da9dfb5cf5090243c8831
Lennart Andersson <LelleAnd@users.noreply.github.com>
2016-09-12 08:51:05 +0200

    C++: Added needed include file (Linux)

--- Cpp/source/OPSObject.cpp
+++ Cpp/source/OPSObject.cpp
@@ -17,6 +17,8 @@
 * You should have received a copy of the GNU Lesser General Public License
 * along with OPS (Open Publish Subscribe).  If not, see <http://www.gnu.org/licenses/>.
 */
+
+#include <string.h>
 #include "OPSTypeDefs.h"
 #include "OPSObject.h"
 
