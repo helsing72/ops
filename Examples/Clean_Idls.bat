9204a2a3066fad345176a939ca9c20f1031cdcff
Lennart Andersson <LelleAnd@users.noreply.github.com>
2016-12-11 11:56:44 +0100

    Updated CMakeLists and added a bat-file for cleaning generated idl's

---
+++ Examples/Clean_Idls.bat
@@ -0,0 +1,10 @@
+@pushd %~dp0
+rmdir /s /q OPSIdls\ChatExample\Generated
+rmdir /s /q OPSIdls\HelloRequestReply\Generated
+rmdir /s /q OPSIdls\HelloWorld\Generated
+rmdir /s /q OPSIdls\MultiLibs\BaseIDLs\Generated
+rmdir /s /q OPSIdls\MultiLibs\DerivedIDLs\Generated
+rmdir /s /q OPSIdls\PizzaProject\Generated
+rmdir /s /q OPSIdls\TestAll\Generated
+rmdir /s /q OPSIdls\WeatherStationExample\Generated
+@popd
