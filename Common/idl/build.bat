9c01b685f5ae75a57a3d5d92100ca71c96ae305e
Lennart Andersson <lelle@localhost>
2019-04-29 23:24:11 +0200

    Updated builds to use relative paths for generated Ada project files

--- Common/idl/build.bat
+++ Common/idl/build.bat
@@ -1,3 +1,3 @@
 @pushd %~dp0
-@call ..\..\Tools\opsc\opsc.bat -g ALL -B ALL -o Generated *.idl -p opsidls
+@call ..\..\Tools\opsc\opsc.bat -g ALL -B ALL -gpr ../../../../Ada/ -o Generated *.idl -p opsidls
 @popd
