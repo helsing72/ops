2c87f6a695c36cf4a57fb5bcb0fa5c5833f46b24
Lennart Andersson <LelleAnd@users.noreply.github.com>
2019-09-29 23:30:59 +0200

    UnitTests, C++: Moved memory_pool() tests to a separate program

--- UnitTests/OPStest-C++/pizzatest-xml.sh
+++ UnitTests/OPStest-C++/pizzatest-xml.sh
@@ -4,6 +4,7 @@ cd ../../
 exit_count=0
 exit_gtest=0
 exit_gtest_mp=0
+exit_gtest_mp3=0
 exit_des_ser=0
 exit_normal=0
 exit_tcp=0
@@ -17,6 +18,8 @@ build.debug/UnitTests/OPStest-C++/gtest/gtest-main --gtest_output="xml:unittest-
 exit_gtest=$?
 build.debug/UnitTests/OPStest-C++/gtest/gtest-mempool --gtest_output="xml:unittest-c++-gtest-mp-result.xml"
 exit_gtest_mp=$?
+build.debug/UnitTests/OPStest-C++/gtest/gtest-mempool3 --gtest_output="xml:unittest-c++-gtest-mp-result.xml"
+exit_gtest_mp3=$?
 
 build.debug/UnitTests/OPStest-C++/UnitTests/test-serialize-and-deserialize --gtest_output="xml:unittest-c++-ser-deser-result.xml"
 exit_des_ser=$?
@@ -49,7 +52,7 @@ bash
 	cppcheck --enable=all --inconclusive --xml --xml-version=2 Cpp/source Cpp/include 2> cppcheck-c++-result.xml
 exit
 
-exit_count=$(( $exit_gtest + $exit_gtest_mp + $exit_des_ser + $exit_normal + $exit_tcp  + $exit_udp ))
+exit_count=$(( $exit_gtest + $exit_gtest_mp + $exit_gtest_mp3 + $exit_des_ser + $exit_normal + $exit_tcp  + $exit_udp ))
 
 if [ $exit_count == 0 ]
 then
--- UnitTests/OPStest-C++/pizzatest.sh
+++ UnitTests/OPStest-C++/pizzatest.sh
@@ -5,6 +5,7 @@ cd ../../
 exit_count=0
 exit_gtest=0
 exit_gtest_mp=0
+exit_gtest_mp3=0
 exit_des_ser=0
 exit_normal=0
 exit_tcp=0
@@ -18,6 +19,8 @@ build.debug/UnitTests/OPStest-C++/gtest/gtest-main
 exit_gtest=$?
 build.debug/UnitTests/OPStest-C++/gtest/gtest-mempool
 exit_gtest_mp=$?
+build.debug/UnitTests/OPStest-C++/gtest/gtest-mempool3
+exit_gtest_mp3=$?
 
 build.debug/UnitTests/OPStest-C++/UnitTests/test-serialize-and-deserialize
 exit_des_ser=$?
@@ -47,7 +50,7 @@ mkdir -p UnitTests/OPStest-C++/Unit_test_results/Coverage
 gcovr -r . --html --html-details -o UnitTests/OPStest-C++/Unit_test_results/Coverage/coverage_result_OPS.html
 
 cppcheck --enable=all Cpp/source Cpp/include 2> UnitTests/OPStest-C++/Unit_test_results/SA_ERRORS.txt > /dev/null
-exit_count=$(( $exit_gtest + $exit_gtest_mp + $exit_des_ser + $exit_normal + $exit_tcp  + $exit_udp ))
+exit_count=$(( $exit_gtest + $exit_gtest_mp + $exit_gtest_mp3 + $exit_des_ser + $exit_normal + $exit_tcp  + $exit_udp ))
 
 if [ $exit_count == 0 ]
 then
